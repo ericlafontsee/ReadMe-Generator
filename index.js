e1708891b3f283ccf14107b1c60dfb4828a1a54a
Eric LaFontsee <elafontsee@gmail.com>
2020-09-10 09:48:00 -0500

    Added prompts for user to answer, created variables to store their results, and create a write to file function

--- index.js
+++ index.js
@@ -1,4 +1,82 @@
-`var inquirer = require("inquirer");
+var inquirer = require("inquirer");
+var fs = require('fs');
+
+inquirer
+    .prompt([{
+            type: "input",
+            message: "What is your project title?",
+            name: "Title"
+        },
+        {
+            type: "input",
+            message: "Provide a description",
+            name: "Description"
+        },
+        {
+            type: "input",
+            message: "What are the installation instructions?",
+            name: "Installation"
+        },
+        {
+            type: "list",
+            message: "What type of license did you use?",
+            name: "License",
+            choices: [
+                "MIT",
+                "GPL",
+                "Apache 2.0",
+                "BSD"
+            ]
+        },
+        {
+            type: "input",
+            message: "Please enter any Usage Information",
+            name: "Usage"
+        },
+        {
+            type: "input",
+            message: "Please enter any Contribution Guidelines",
+            name: "Contributors"
+        },
+        {
+            type: "input",
+            message: "Please enter any Test Instructions.",
+            name: "Testing"
+        }
+
+    ]).then(function(data) {
+        console.log(data);
+        const title = data.Title;
+        const description = data.Description;
+        const installation = data.installation;
+        const license = data.License;
+        const usage = data.Usage;
+        const contributors = data.Contributors;
+        const testing = data.Testing;
+        const filename = title.toLowerCase().split(' ').join('') + ".json";
+
+        var result = (`
+        # ${title} 
+        
+        ${description}
+        * [Installation](#Installation)
+        * [License](#License)
+        * [Contributors](#Contributors)
+        * [Author](#Author)
+        * [Tests](#Tests)
+        ## Installation
+        ${installation}
+        ## License 
+        This project is licensed under the ${license}.
+        ## Contributors
+        ${contributors}
+        
+        ## Tests
+        ${testing}
+       
+                    `);
+        writeToFile(filename, data);
+    });
 
 // array of questions for user
 const questions = [
@@ -6,7 +84,17 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(filename, result, function(err) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+
+    });
+}
 
 // function to initialize program
 function init() {
